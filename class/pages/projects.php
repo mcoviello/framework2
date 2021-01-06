da7668a63b1e790e5798829b19fbff4ad0b4c8ff
mcoviello18 <mcnika1234@gmail.com>
2021-01-06 17:26:45 +0000

    Added an 'Add Project' button
    added an add project button that opens a modal which contains a form.

--- class/pages/projects.php
+++ class/pages/projects.php
@@ -25,7 +25,25 @@
  */
         public function handle(Context $context)
         {
-            $context->local()->message(\Framework\Local::MESSAGE, 'bruh');
+            $fdt = $context->formdata('post');
+            if($fdt->exists('title') && $fdt->exists('desc'))
+            {
+                $user = $context->user();
+                $title = $fdt->mustfetch('title');
+                $desc = $fdt->mustfetch('desc');
+                $priv = ($fdt->mustfetch('privc'));
+
+                if($title !== '' && $desc !== '')
+                {
+                    $project = R::dispense('project');
+                    $project->title = $title;
+                    $project->description = $desc;
+                    $project->private = $priv;
+                    //$user->noLoad()->ownProject[] = $project;
+                    //R::store($user);
+                    $context->local()->message(\Framework\Local::MESSAGE, 'Project Successfully Created'); 
+                }
+            }
             return '@content/projects.twig';
         }
     }
